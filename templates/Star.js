af6d93c47b5f7afa743c3879fa59134e30473b7f
andreizad <48527347+andreizad@users.noreply.github.com>
2021-03-20 10:06:12 -0400

    Delete templates directory

--- templates/Star.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("input:radio:checked").data("chk",true);
-    $("input:radio").click(function(){
-        $("input[name='"+$(this).attr("name")+"']:radio").not(this).removeData("chk");
-        $(this).data("chk",!$(this).data("chk"));
-        $(this).prop("checked",$(this).data("chk"));
-    });
-});
